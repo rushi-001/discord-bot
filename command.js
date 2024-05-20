93732641c4d4e2df14bc4e057ce346f2de8bb434
RUSHI PANCHAL <panchalrushi2182004@gmail.com>
2024-05-20 20:07:01 +0530

    command help and create added in create URL fatching complited.

---
+++ command.js
@@ -0,0 +1,31 @@
+const { REST, Routes, ApplicationCommandOptionType } = require("discord.js")
+
+const commands = [
+    {
+        name: "help",
+        description: "Show's how to use the bot."
+    },
+    {
+        name: "create",
+        description: "Create short URL using given URL.",
+        options: [
+            {
+                name: 'url',
+                description: 'The URL to use for short URL creation.',
+                type: ApplicationCommandOptionType.String,
+                required: true,
+            }
+        ]
+    }
+]
+
+const rest = new REST({ version: "10" }).setToken("MTI0MjA3ODU1NDMyMjIzOTYyMA.GYzlSN.T3Rw6DOD_rqV3aVnryWpNZbDR6s6gF_gRGmU7w");
+
+(async () => {
+    try {
+        await rest.put(Routes.applicationCommands("1242078554322239620"), { body: commands });
+        console.log("Applicaiton connected (/)");
+    } catch (error) {
+        console.error(error)
+    }
+})();
---
+++ index.js
@@ -0,0 +1,26 @@
+const { Client, GatewayIntentBits } = require("discord.js")
+
+const token = "MTI0MjA3ODU1NDMyMjIzOTYyMA.GYzlSN.T3Rw6DOD_rqV3aVnryWpNZbDR6s6gF_gRGmU7w"
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+})
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'help') {
+        await interaction.reply("+------------------------------------------+ \n✔ It's an URL shortner Bot which create shor URL like [ bitly.com ]. \n✔ This project is available on my **Github [ https://github.com/rushi-001 ]**. \n+------------------------------------------+ \n✔ To create Short URL use ~> **/create [url https://www.example.com]**.");
+    }
+
+    if (interaction.commandName === 'create') {
+        const url = interaction.options.getString('url')
+        await interaction.reply(`🔗 test: ${url}`);
+    }
+});
+
+client.login(token);
